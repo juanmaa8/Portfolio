0651b4fb3f7ecd0320f55648e6d443099657710a
juanmaa8 <83790062+juanmaa8@users.noreply.github.com>
2025-12-16 08:34:49 -0500

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface Project {
+  id: number;
+  category: string;
+  title: string;
+  client: string;
+  metrics: string;
+  impactBadges?: string[];
+  tags: string[];
+  gallery?: string[];
+  shortDesc: string;
+  challenge: string;
+  solution: string;
+  results: string[];
+  stack: string[];
+}
